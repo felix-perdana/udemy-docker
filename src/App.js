4df1b84a15a7e0aa3dc06f5e0dac648ab2a8e612
Felix Perdana <felixperdana@Travelokadmins-MacBook-Pro.local>
2020-05-31 17:40:56 +0700

    initial commit

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
+          Felix has edited this the third time.
         </p>
         <a
           className="App-link"
